ae15ee8995566f5bb083ab6fed4f75aec733ba27
Thomas Hahn <Thahn@tcbtech.com>
2021-04-02 01:00:54 -0700

    Genericized

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/gauntletwizard/awsescurl
 
 go 1.14
 
-require github.com/aws/aws-sdk-go v1.38.11 // indirect
+require github.com/aws/aws-sdk-go v1.38.11
--- main.go
+++ main.go
@@ -5,37 +5,58 @@ package main
  */
 
 import (
+	"flag"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	"io"
+	"log"
 	"net/http"
-	"strings"
+	"os"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
+)
+
+var (
+	method  = flag.String("X", "GET", "Method (from curl's -X)")
+	bodyloc = flag.String("f", "-", "File to read (defaults to stdin)")
 )
 
 func main() {
 
+	flag.Parse()
+
 	// Basic information for the Amazon Elasticsearch Service domain
-	domain := "" // e.g. https://my-domain.region.es.amazonaws.com
-	index := "my-index"
-	id := "1"
-	endpoint := domain + "/" + index + "/" + "_doc" + "/" + id
-	region := "" // e.g. us-east-1
+	endpoint := flag.Arg(0)
 	service := "es"
 
-	// Sample JSON document to be included as the request body
-	json := `{ "title": "Thor: Ragnarok", "director": "Taika Waititi", "year": "2017" }`
-	body := strings.NewReader(json)
+	// Read in the request body
+	var body io.ReadSeeker
+	var err error
+	if *bodyloc == "-" {
+		log.Fatal("Unimplemented, you must specify a file")
+	} else {
+		body, err = os.Open(*bodyloc)
+	}
+	if err != nil {
+		log.Fatalf("Failed to read request body %s", err)
+	}
 
 	// Get credentials from environment variables and create the AWS Signature Version 4 signer
-	credentials := credentials.NewEnvCredentials()
-	signer := v4.NewSigner(credentials)
+	config, err := session.NewSession()
+	if err != nil {
+		log.Fatalf("Error getting AWS Credentials %s", err)
+	}
+	creds := config.Config.Credentials
+	region := config.Config.Region
+
+	signer := v4.NewSigner(creds)
 
 	// An HTTP client for sending the request
 	client := &http.Client{}
 
 	// Form the HTTP request
-	req, err := http.NewRequest(http.MethodPut, endpoint, body)
+	req, err := http.NewRequest(*method, endpoint, body)
 	if err != nil {
 		fmt.Print(err)
 	}
@@ -44,10 +65,11 @@ func main() {
 	req.Header.Add("Content-Type", "application/json")
 
 	// Sign the request, send it, and print the response
-	signer.Sign(req, body, service, region, time.Now())
+	signer.Sign(req, body, service, *region, time.Now())
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Print(err)
 	}
 	fmt.Print(resp.Status + "\n")
+	io.Copy(os.Stdout, resp.Body)
 }
